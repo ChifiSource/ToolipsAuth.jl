da1d0a8e7b0472a2b83a5903be5dd651cb9ce48e
emmy <52672675+emmettgb@users.noreply.github.com>
2022-09-15 02:19:55 -0500

    doc + completion :)

--- src/ToolipsAuth.jl
+++ src/ToolipsAuth.jl
@@ -11,7 +11,6 @@ Provides simple authentication for toolips with client tokens.
 """
 module ToolipsAuth
 using Toolips
-import OddStructures: AbstractDimensions
 import Toolips: ServerExtension, AbstractConnection
 using ToolipsSession
 using ToolipsSession: gen_ref
@@ -53,12 +52,12 @@ provide_tokens::Bool = true, bit::Int64 = 16)
 mutable struct Auth <: ServerExtension
     active_routes::Vector{String}
     host::String
-    port::String
+    port::Int64
     type::Vector{Symbol}
     f::Function
     server_data::Dict{Symbol, Any}
-    client_groups::Dict{String, Vector{Vector{UInt8}}}
-    clients::Dict{Vector{UInt8}, String}
+    client_groups::Dict{String, Vector{String}}
+    client_tokens::Dict{Vector{UInt8}, String}
     bit::Int64
     function Auth(active_routes::Vector{String} = ["/"], host::String = "127.0.0.1",
         port::Int64 = 8000;
@@ -67,74 +66,164 @@ mutable struct Auth <: ServerExtension
         client_groups = Dict{String, Vector{String}}()
         server_data = Dict{Symbol, Any}(:provide_tokens => true,
         :tokenname => tokenname)
-        client_tokens = Dict{UInt8, String}()
+        client_tokens = Dict{Vector{UInt8}, String}()
         f(c::Connection) = begin
-            fullpath::String = http.message.target
-            if contains(http.message.target, "?")
-                fullpath = split(http.message.target, '?')[1]
+            fullpath::String = c.http.message.target
+            if contains(c.http.message.target, "?")
+                fullpath = split(c.http.message.target, '?')[1]
             end
             if ~(fullpath in active_routes)
                 return
             end
-            token::String = ""
+            tok::String = ""
             args::AbstractDict = getargs(c)
             if :key in keys(getargs(c))
-                token = args[:key]
+                tok = args[:key]
             else
-                token = token!(c)
+                tok = token!(c)
             end
             if server_data[:provide_tokens] == true
-                write!(c, token)
+                write!(c, token(tokenname, text = tok))
             end
             on(c, "unload") do cm::ComponentModifier
 
             end
         end
-        new(host, port, [:func, :connection], f, server_data )::AuthData
+        new(active_routes, host, port, [:func, :connection], f, server_data, client_groups,
+         client_tokens, bit)::Auth
     end
 end
 
+"""
+**Toolips Auth**
+### client_token(c::AbstractConnection) -> ::String
+------------------
+Returns the current client's token.
+#### example
+```
+
+```
+"""
 function client_token(c::AbstractConnection)
-    token = c[:Auth].clients[sha256(getip(c))]
+    tok = c[:Auth].client_tokens[sha256(getip(c))]
 end
 
+"""
+**Toolips Auth**
+### group!(c::AbstractConnection, s::String = "public"; reset::Bool = false)
+------------------
+groups the client into usergroup `s`. Reset as true will also erase all old
+usergroups.
+#### example
+```
+
+```
+"""
 function group!(c::AbstractConnection, s::String = "public"; reset::Bool = false)
-    groups = c[:Auth].client_groups
     if reset
-        groups = Dict{String, String}()
+        c[:Auth].client_groups[client_token(c)] = [s]
+        return
     end
-    push!(groups[client_token(c)], s)
+    push!(c[:Auth].client_groups[client_token(c)], s)
 end
 
-in_group(c::Connection, group::String) = group in group(c)
+"""
+**Toolips Auth**
+### in_group(c::Connection, group::String) -> ::Bool
+------------------
+Checks if Connection is in group `group`.
+#### example
+```
 
-group(c::Connection) = c[:Auth].client_groups[client_token(c)][:group]
+```
+"""
+in_group(c::Connection, name::String) = name in group(c)
 
+"""
+**Toolips Auth**
+### group(c::Connection) -> ::Vector{String}
+------------------
+Gets the groups of the current client.
+#### example
+```
+
+```
+"""
+group(c::Connection) = c[:Auth].client_groups[client_token(c)]
+
+"""
+**Toolips Auth**
+### group(f::Function, c::AbstractConnection, s::String) -> _
+------------------
+Provide a function and the function will only be performed if `c` is in
+    group `s`.
+#### example
+```
+myr = route("/") do c::Connection
+    group(c, "public") do c::Connection
+        write!(c, "in public!")
+    end
+    group(c, "first") do c::Connection
+        write!(c, "this is your first visit!")
+    end
+end
+```
+"""
 group(f::Function, c::Connection, s::String) = begin
     if in_group(c, s)
         f(c)
     end
 end
 
+"""
+**Toolips Auth**
+## token!(c::AbstractConnection) -> ::String
+------------------
+Returns a client's token OR gives a client a new token.
+#### example
+```
+
+```
+"""
 token!(c::AbstractConnection) = begin
     bit = c[:Auth].bit
-    token::String = ""
-    if sha256(getip(c)) in keys(c[:Auth].clienttokens)
+    tok::String = ""
+    if sha256(getip(c)) in keys(c[:Auth].client_tokens)
         tokentext = c[:Auth].client_tokens[sha256(getip(c))]
     else
-        token = join([gen_ref() for r in 1:bit/16])
-        c[:Auth].client_tokens[sha256(getip(c))] = token
-        group!(c, "new")
+        tok = join([gen_ref() for r in 1:bit/16])
+        c[:Auth].client_tokens[sha256(getip(c))] = tok
+        group!(c, "new", reset = true)
     end
-    token
+    tok
 end
 
+"""
+**Toolips Auth**
+### token(name::String, p::Pair{String, Any} ..., args ...) -> ::Component{:token}
+------------------
+Builds the token `Component`.
+#### example
+```
+
+```
+"""
 function token(name::String, p::Pair{String, Any} ...; args ...)
-    c::Component{:token} = Component(name, "token", p ... args ...)
+    c::Component{:token} = Component(name, "token", p ..., args ...)
     style!(c, "display" => "none")
     c::Component{:token}
 end
 
+"""
+**Toolips Auth**
+### auth_redirect!(c::Connection, cm::ComponentModifier, s::String, delat::Number = .5)
+------------------
+Redirects the client with their token as an argument.
+#### example
+```
+
+```
+"""
 function auth_redirect!(c::Connection,
     cm::ComponentModifier, s::String, delay::Number = .5)
     key = cm[c[:Auth].server_data[:tokenname]]["text"]
@@ -142,5 +231,5 @@ function auth_redirect!(c::Connection,
     redirect!(cm, url, delay)
 end
 
-export token, token!, sha256, Auth
+export token, token!, sha256, Auth, group, group!, in_group, client_token
 end # module
