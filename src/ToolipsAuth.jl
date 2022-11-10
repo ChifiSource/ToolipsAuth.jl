ae4aa03a6f700d8b1289a4161f0619adcaa2bb16
emmy <52672675+emmettgb@users.noreply.github.com>
2022-11-10 04:51:17 -0600

    adding a more robust group system
    along with client data being in a better structure -- very much so the user can put whatever into there. Next, I will create some sort of IO likely with SHA to create automatic saving and loading of dictionary portions -- but I might just wait for oddframes and streamframes

--- src/ToolipsAuth.jl
+++ src/ToolipsAuth.jl
@@ -33,8 +33,17 @@ well as data now the server can just store. All in all, this is a very useful
 extension. This could be used as the basis for a " login with Google"
 implementation, even. `data` holds server data, whereas `token_data` holds client
 data. `bit` describes how many bits, divisible by 16, the tokens should be.
-##### example
+##### examples
+- **client tokens**
+
+The following example demonstrates logging the client's token into console `on`
+load with ToolipsSession:
 ```
+using Toolips
+using ToolipsSession
+using ToolipsAuth
+
+
 function prtoken(c::Connection)
     on(c, "load") do cm::ComponentModifier
         c[:Logger].log(cm["mytoken"]["text"])
@@ -43,6 +52,53 @@ end
 
 server = WebServer(extensions = [Auth("mytoken"), Logger()], routes = [Route("/", prtoken)])
 server.start()
+```
+Here is another example, were we build a simple login by using SHA to store login
+details inside of our outer-module scope. Note that this requires an additional
+change to our `dev.jl` and/or `prod.jl` environments.
+- add to **dev.jl**:
+```
+HOSTNAME = "127.0.0.1"
+USERS = Dict{String, Vector{UInt8}}()
+```
+These will be our globally defined variables, if we wanted to we could even task
+a system to save this with `JLD2`, write it into some data format, push to a db,
+what have you. This will soon be made easier, as well, with the refinement of
+`ToolipsRemote` onto `ToolipsManager`. Back to the project, we need our route to
+allow new users to provide user data. For now it will just be a username and a
+password, hence how the `Dict` is so simple.
+- rewrite **home** in your source file:
+```
+using Toolips
+using ToolipsSession
+using ToolipsAuth
+using ToolipsDefaults: textdiv, sheet
+
+home = route("/") do c::Connection
+    write!(c, sheet())
+    group(c, "new") do c::Connection
+        # username/pwd box
+        usernamebox = textdiv("usernamebox")
+        pwdbox = textdiv("mytxtdiv")
+        my_logo = h(1, "mylogo", text = "The Pink Balloon Club")
+        submitbutton = button("submitbutton" text = "login")
+        on(c, submitbutton, "click") do cm::ComponentModifier
+
+        end
+        lower_div = div("lowrdi", align = "center")
+        upper_div = div("upprdi", align = "center")
+        style!(upper_div, "margin-top" => 50percent, "padding" => 20px,
+        background-color => #84F04F)
+        bod = body("main-body")
+        push!(bod, upper_div, lower_div)
+        write!(c, )
+    end
+end
+
+register = route("/") do c::Connection
+    write!(c, sheet())
+
+end
 ```
 ------------------
 ##### constructors
@@ -56,18 +112,23 @@ mutable struct Auth <: ServerExtension
     type::Vector{Symbol}
     f::Function
     server_data::Dict{Symbol, Any}
-    client_groups::Dict{String, Vector{String}}
+    authlinks::Dict{String, Vector{String}}
+    client_info::Dict{String, Dict{String, Any}}
     client_tokens::Dict{Vector{UInt8}, String}
     bit::Int64
     function Auth(active_routes::Vector{String} = ["/"], host::String = "127.0.0.1",
         port::Int64 = 8000;
         tokenname::String = "auth-token", bit::Int64 = 16,
-        newconnections::Symbol = :public)
-        client_groups = Dict{String, Vector{String}}()
+        default_group::String = "public", save::Bool = false)
+        client_info = Dict{String, Vector{String}}()
         server_data = Dict{Symbol, Any}(:provide_tokens => true,
         :tokenname => tokenname)
         client_tokens = Dict{Vector{UInt8}, String}()
+        authlinks::Dict{String, Vector{String}} = Dict{String, Vector{String}}()
         f(c::Connection) = begin
+            if save == true
+                
+            end
             fullpath::String = c.http.message.target
             if contains(c.http.message.target, "?")
                 fullpath = split(c.http.message.target, '?')[1]
@@ -77,8 +138,12 @@ mutable struct Auth <: ServerExtension
             end
             tok::String = ""
             args::AbstractDict = getargs(c)
-            if :key in keys(getargs(c))
+            if :key in keys(args)
                 tok = args[:key]
+                if tok in authlinks
+                    delete!(authlinks, tok)
+                    client_tokens[sha256(getip(c))] = tok
+                end
             else
                 tok = token!(c)
             end
@@ -86,11 +151,15 @@ mutable struct Auth <: ServerExtension
                 write!(c, token(tokenname, text = tok))
             end
             on(c, "unload") do cm::ComponentModifier
-
+                #== TODO Store authenticated data, remove the current key
+                    Perhaps allow a closure to run? I am not exactly sure
+                    where I want to take it. :D
+                ==#
             end
         end
-        new(active_routes, host, port, [:func, :connection], f, server_data, client_groups,
-         client_tokens, bit)::Auth
+        new(active_routes, host, port, [:func, :connection], f, server_data,
+        authlinks, client_groups,
+        client_tokens, bit)::Auth
     end
 end
 
@@ -231,5 +300,43 @@ function auth_redirect!(c::Connection,
     redirect!(cm, url, delay)
 end
 
+
+"""
+**Toolips Auth**
+### auth_link!(c::Connection, group::Vector{String}, hostname::String)
+------------------
+Generates an authenticated link for hostname and returns key to give that group.
+#### example
+Here we write a link that auto authenticates. Alternatively, we
+could also use `group!` here. Try `?(group!)` and `?(group)` for more
+information on this.
+Another alternative is group! and/or auth_redirect! on a `ComponentModifier.`
+```
+using Toolips
+using ToolipsAuth
+#== This would ideally be an environmental variable,
+   You could put this into dev.jl if you wanted to ! ==#
+HOST = "127.0.0.1"
+home = route("/") do c::Connection
+    # If the user is new to the server by IP, then we serve them this:
+    group(c, "new") do c::Connection
+        write!(c, "you're not authenticated, click this link: ")
+        write!(c, auth_link!(c, ["user"], HOST))
+    end
+    # If they are authenticated via this link, we serve them this:
+    group(c, "user") do c::Connection
+        write!(c, "now you are authenticated.")
+    end
+end
+```
+"""
+function auth_link!(c::Connection, group::Vector{String},
+    hostname::String)
+    key::String = join([gen_ref() for r in 1:c[:Auth].bit/16])
+    c[:Auth].authlinks[key] = group
+    return("https://$hostname/?key=$key")
+end
+
 export token, token!, sha256, Auth, group, group!, in_group, client_token
+export auth_link!, auth_redirect!
 end # module
